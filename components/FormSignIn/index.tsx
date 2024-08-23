0f910a5436010dd03b9967449dd6268ee4fc87e0
Volha Melayok <neiwra@gmail.com>
2024-08-23 09:02:01 +0200

    feat: add basic layout for sign in form

--- components/FormSignIn/index.tsx
+++ components/FormSignIn/index.tsx
@@ -1,9 +1,21 @@
 'use client';
 
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { SignInData } from '@/interfaces/signin.interface';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationSchema } from '@/validation';
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  FormHelperText,
+  TextField,
+} from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function FormSignIn() {
   const {
@@ -18,14 +30,50 @@ function FormSignIn() {
     console.log(data);
   };
 
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <form onSubmit={handleSubmit(onFormSubmit)}>
-      <label htmlFor="email">Login</label>
-      <input type="email" id="login" {...register('login')} />
-      {errors.login && <div className="error">{errors.login.message}</div>}
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" {...register('password')} />
-      {errors.password && <div className="error">{errors.password.message}</div>}
+      <div>
+        <TextField
+          error={!!errors.login}
+          required
+          id="login"
+          label="Login"
+          type="email"
+          helperText={errors.login ? errors.login.message : ''}
+          {...register('login')}
+        />
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" error={!!errors.password}>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            autoComplete="current-password"
+            {...register('password')}
+          />
+          <FormHelperText>{errors.password ? errors.password.message : ''}</FormHelperText>
+        </FormControl>
+      </div>
       <button type="submit" disabled={!isValid && isSubmitted}>
         Sign in
       </button>
