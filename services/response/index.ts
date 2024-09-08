3c6076e181459312f343db5c78c7ccf73f920845
Shadman <shadman.alizada@gmail.com>
2024-09-08 11:31:27 +0400

    feat: added fetcher restclient service fn

---
+++ services/response/index.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { getErrorMessage } from '@/utils/getErrorMessage';
+
+const ignoredBodyMethods = ['GET', 'HEAD'];
+
+export async function fetcher(url: string | URL | Request, options?: RequestInit) {
+  try {
+    const startTimer = Date.now();
+
+    const body = options?.body && !ignoredBodyMethods.includes(options.method || '') ? options.body : undefined;
+
+    const res = await fetch(url, {
+      ...options,
+      body,
+    });
+
+    const data = await res.json();
+
+    const endTimer = Date.now();
+
+    return {
+      data: JSON.stringify(data, null, 2),
+      status: res.status,
+      statusText: res.statusText,
+      parsedHeaders: Object.fromEntries(res.headers),
+      success: res.ok,
+      responseTime: endTimer - startTimer,
+    };
+  } catch (error) {
+    return {
+      data: JSON.stringify(error),
+      status: 500,
+      statusText: getErrorMessage(error, 'Failed to fetch'),
+      parsedHeaders: {},
+      success: false,
+      responseTime: 0,
+    };
+  }
+}
