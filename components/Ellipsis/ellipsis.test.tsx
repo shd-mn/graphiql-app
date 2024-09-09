3d8acf2826d9fa0a9ba50b022787d74b69ee424a
Shadman <shadman.alizada@gmail.com>
2024-09-09 22:21:07 +0400

    test: add Ellipsis Component test

---
+++ components/Ellipsis/ellipsis.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Ellipsis from '.';
+
+describe('Ellipsis Component', () => {
+  it('renders the loading svg', () => {
+    render(<Ellipsis />);
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+
+    const img = screen.getByAltText('loader');
+    expect(img).toBeInTheDocument();
+
+    screen.debug();
+  });
+});
