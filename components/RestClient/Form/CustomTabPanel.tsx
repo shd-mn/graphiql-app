9074592bb18fb9ffbe4cdccf88ed911db04f5b7f
Shadman <shadman.alizada@gmail.com>
2024-09-12 13:32:05 +0400

    feat: add table for res headers, some improvment in monaco editor

--- components/RestClient/Form/CustomTabPanel.tsx
+++ components/RestClient/Form/CustomTabPanel.tsx
@@ -4,20 +4,22 @@ interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
+  className?: string;
 }
 
 function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, className, ...other } = props;
 
   return (
     <div
+      className={className}
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box>{children}</Box>}
+      {value === index && <Box className="h-full w-full">{children}</Box>}
     </div>
   );
 }
--- components/RestClient/Response/ResponseBody.tsx
+++ components/RestClient/Response/ResponseBody.tsx
@@ -1,8 +1,9 @@
 import { selectResponse } from '@/redux/features/mainSlice';
 import { useAppSelector } from '@/redux/hooks';
 import Editor from '@monaco-editor/react';
-import { Box } from '@mui/material';
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import CustomTabPanel from '../Form/CustomTabPanel';
+import { useMemo } from 'react';
 
 type PropTypes = {
   activeTab: number;
@@ -11,20 +12,41 @@ type PropTypes = {
 function ResponseBody({ activeTab }: PropTypes) {
   const { data, parsedHeaders } = useAppSelector(selectResponse);
 
+  const headers = Object.entries(parsedHeaders).map(([key, value]) => ({
+    key,
+    value,
+  }));
+
+  const editorLanguage = useMemo(() => {
+    const contentType = parsedHeaders['content-type'] || '';
+    if (contentType.includes('application/json')) {
+      return 'json';
+    } else if (contentType.includes('text/html')) {
+      return 'html';
+    } else if (contentType.includes('text/xml')) {
+      return 'xml';
+    }
+    return 'text';
+  }, [parsedHeaders]);
+
   return (
-    <Box component="div">
-      <CustomTabPanel value={activeTab} index={0}>
+    <Box component="div" className="mb-1 flex h-full flex-col">
+      <CustomTabPanel value={activeTab} index={0} className="h-full flex-grow">
         <Editor
-          height="500px"
-          defaultLanguage="json"
           theme="vs-dark"
-          defaultValue={data}
+          defaultLanguage={editorLanguage}
+          value={data}
+          loading={null}
           options={{
             readOnly: true,
             minimap: {
               enabled: false,
             },
             smoothScrolling: true,
+            scrollbar: {
+              verticalScrollbarSize: 12,
+              horizontalScrollbarSize: 12,
+            },
             lineNumbersMinChars: 4,
             wordSeparators: '~!@#$%^&*()-=+[{]}|;:\'",.<>/?',
             wordWrap: 'on',
@@ -35,10 +57,28 @@ function ResponseBody({ activeTab }: PropTypes) {
           }}
         />
       </CustomTabPanel>
-      <CustomTabPanel value={activeTab} index={1}>
-        {JSON.stringify(parsedHeaders)}
+      <CustomTabPanel value={activeTab} index={1} className="h-full flex-grow overflow-auto">
+        <TableContainer component={Paper} className="h-full">
+          <Table stickyHeader aria-label="response headers table">
+            <TableHead>
+              <TableRow>
+                <TableCell className="w-full">Key</TableCell>
+                <TableCell className="w-full">Value</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {headers.map(({ key, value }) => (
+                <TableRow key={key} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell className="w-full">{key}</TableCell>
+                  <TableCell className="w-full">{value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </CustomTabPanel>
     </Box>
   );
 }
+
 export default ResponseBody;
