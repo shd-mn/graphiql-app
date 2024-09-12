44bdf81e1dac64150d9edda3e67918d07218d7b4
Shadman <shadman.alizada@gmail.com>
2024-09-12 13:37:22 +0400

    fix: add z-index to ensure proper layering and customizable appearance

--- components/Resizable/ResizableHandle.tsx
+++ components/Resizable/ResizableHandle.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { ComponentProps } from 'react';
 import { PanelResizeHandle } from 'react-resizable-panels';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 interface ResizableHandleProps extends ComponentProps<typeof PanelResizeHandle> {
   direction?: 'vertical' | 'horizontal';
@@ -10,13 +9,16 @@ interface ResizableHandleProps extends ComponentProps<typeof PanelResizeHandle> 
 function ResizableHandle({ direction = 'vertical', ...props }: ResizableHandleProps) {
   return (
     <PanelResizeHandle
-      className={`flex items-center justify-center bg-gray-200 ${direction === 'horizontal' ? 'w-[1px]' : 'h-[1px]'}`}
+      className={`z-50 flex items-center justify-center bg-gray-200 py-[1px] ${
+        direction === 'horizontal' ? 'w-[1px]' : 'h-[1px]'
+      }`}
       {...props}
     >
-      <MoreHorizIcon
-        className={`rounded-sm bg-inherit ${direction === 'horizontal' && 'rotate-90'}`}
-        fontSize="small"
-      />
+      <div
+        className={`relative h-1 w-1 rounded-full bg-gray-400 before:absolute before:h-1 before:w-1 before:rounded-full before:bg-gray-400 before:content-[''] after:absolute after:h-1 after:w-1 after:rounded-full after:bg-gray-400 after:content-[''] ${
+          direction === 'horizontal' ? 'before:top-[-6px] after:top-[6px]' : 'before:left-[-6px] after:left-[6px]'
+        } `}
+      ></div>
     </PanelResizeHandle>
   );
 }
