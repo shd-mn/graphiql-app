4618f1db215f27d71c3ec054ebdd7f130777a1c1
Shadman <shadman.alizada@gmail.com>
2024-09-12 13:33:00 +0400

    feat: handle loading, empty, error and succes state in ResponseSection

--- components/RestClient/Response/index.tsx
+++ components/RestClient/Response/index.tsx
@@ -3,29 +3,39 @@
 import { useState } from 'react';
 import { Box } from '@mui/material';
 import { useAppSelector } from '@/redux/hooks';
-import { selectResponse } from '@/redux/features/mainSlice';
+import { selectAllMainState } from '@/redux/features/mainSlice';
 import EmptyState from './EmptyState';
 import ResponseBody from './ResponseBody';
 import ResponseHeader from './ResponseHeader';
+import Ellipsis from '@/components/Ellipsis';
+import ErrorState from './ErrorState';
 
 function ResponseSection() {
   const [activeTab, setActiveTab] = useState(0);
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setActiveTab(newValue);
   };
-  const { data } = useAppSelector(selectResponse);
+  const {
+    response: { data, success, statusText, status },
+    isLoading,
+  } = useAppSelector(selectAllMainState);
+
+  if (isLoading) return <Ellipsis />;
+
+  if (status === 0 && !data && !statusText) {
+    return <EmptyState />;
+  }
+
+  if (!success && status === 0) {
+    return <ErrorState />;
+  }
 
   return (
-    <Box component="section" className="h-full flex-1 px-3">
-      {data ? (
-        <>
-          <ResponseHeader activeTab={activeTab} handleTabChange={handleTabChange} />
-          <ResponseBody activeTab={activeTab} />
-        </>
-      ) : (
-        <EmptyState />
-      )}
+    <Box component="section" className="padding-x flex h-full flex-col">
+      <ResponseHeader activeTab={activeTab} handleTabChange={handleTabChange} />
+      <ResponseBody activeTab={activeTab} />
     </Box>
   );
 }
+
 export default ResponseSection;
