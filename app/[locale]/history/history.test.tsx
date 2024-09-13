ca28d6fcca828cf54d4d86d6bd99a7f314781d53
Shadman <shadman.alizada@gmail.com>
2024-09-13 20:00:12 +0400

    test: add next-intl to test-wrapper

--- app/[locale]/history/history.test.tsx
+++ app/[locale]/history/history.test.tsx
@@ -15,6 +15,9 @@ describe('History Page', () => {
 
     customRender(<History />);
     expect(screen.getByText("You haven't executed any requests")).toBeInTheDocument();
+    expect(screen.getByText("It's empty here. Try:")).toBeInTheDocument();
+    expect(screen.getByText('REST Client')).toBeInTheDocument();
+    expect(screen.getByText('GraphiQL Client')).toBeInTheDocument();
   });
 
   it('renders DataTable when requests are stored', () => {
@@ -25,5 +28,8 @@ describe('History Page', () => {
 
     customRender(<History />);
     expect(screen.queryByText("You haven't executed any requests")).not.toBeInTheDocument();
+    expect(screen.queryByText("It's empty here. Try:")).not.toBeInTheDocument();
+    expect(screen.queryByText('REST Client')).not.toBeInTheDocument();
+    expect(screen.queryByText('GraphiQL Client')).not.toBeInTheDocument();
   });
 });
