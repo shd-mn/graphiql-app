7287b246e95d9cd51a26a523b111e2aa5c87ef91
Volha Melayok <neiwra@gmail.com>
2024-09-15 18:40:59 +0200

    test: add GraphQL variable editor test

---
+++ components/GraphiQLClient/url.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { describe, it, vi, expect } from 'vitest';
+import { FormState, UseFormRegister } from 'react-hook-form';
+import { UrlGraphql } from '@/types/url-graphql.types';
+import { graphiqlSlice } from '@/redux/features/graphiqlSlice';
+import UrlSection from '@/components/GraphiQLClient/UrlSection';
+
+const store = configureStore({
+  reducer: {
+    graphiql: graphiqlSlice.reducer,
+  },
+});
+
+describe('UrlSection Component', () => {
+  const mockRegister = vi.fn() as UseFormRegister<UrlGraphql>;
+
+  const mockErrors: FormState<UrlGraphql>['errors'] = {
+    endpoint: { message: 'Invalid endpoint', type: 'required' },
+    sdl: { message: 'Invalid SDL URL', type: 'pattern' },
+  };
+
+  it('should display error messages for endpoint and SDL when errors are passed', () => {
+    render(
+      <Provider store={store}>
+        <UrlSection register={mockRegister} errors={mockErrors} />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Invalid endpoint')).toBeInTheDocument();
+    expect(screen.getByText('Invalid SDL URL')).toBeInTheDocument();
+  });
+});
