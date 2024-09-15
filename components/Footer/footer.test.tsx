34f33c72463f2287eaca68acb722c35f7467ccdc
Volha Melayok <neiwra@gmail.com>
2024-09-15 09:35:08 +0200

    test: add Footer component test

---
+++ components/Footer/footer.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import Footer from '../../components/Footer/index';
+import '@testing-library/jest-dom';
+
+vi.mock('next/image', () => ({
+  default: ({ alt }: { alt: string }) => <img alt={alt} />,
+}));
+
+describe('Footer Component', () => {
+  it('should render the footer with correct links and content', () => {
+    render(<Footer />);
+
+    expect(screen.getByText('Shadman')).toBeInTheDocument();
+    expect(screen.getByText('Alizada')).toBeInTheDocument();
+    expect(screen.getByText('Volha')).toBeInTheDocument();
+    expect(screen.getByText('Melayok')).toBeInTheDocument();
+    expect(screen.getByText('2024')).toBeInTheDocument();
+
+    expect(screen.getByText('RS School')).toBeInTheDocument();
+    expect(screen.getByAltText('The Rolling Scopes School')).toBeInTheDocument();
+  });
+});
