5044f2d3d98139f8947d3fdaffcef999eeccaa6a
Volha Melayok <neiwra@gmail.com>
2024-09-15 15:08:01 +0200

    feat: add header sticky functionality

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,13 +4,14 @@ import { Link, useRouter } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, logout } from '@/firebase';
-import { Button } from '@mui/material';
 import { toast } from 'sonner';
 import { routes } from '@/constants/routes';
 import LanguageSwitcher from '../UI/LanguageSwitcher';
+import { useState } from 'react';
 
 function Header() {
   const [user] = useAuthState(auth);
+  const [isSticky, setIsSticky] = useState(false);
 
   const router = useRouter();
   const t = useTranslations('Header');
@@ -26,24 +27,43 @@ function Header() {
     }
   };
 
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
+  if (typeof window !== 'undefined') {
+    window.addEventListener('scroll', handleScroll);
+  }
+
   return (
-    <header className="padding-x z-50 flex h-14 items-center justify-between bg-blue-600 text-white">
+    <header
+      className={`padding-x sticky inset-x-0 top-0 z-50 flex h-14 items-center justify-between bg-blue-600 text-white ${isSticky ? 'bg-gray-500' : ''}`}
+    >
       <div>
-        <Link href={routes.home}>{t('home')}</Link>
+        <Link href={routes.home} className="custom-button">
+          {t('home')}
+        </Link>
       </div>
       <div className="flex items-center gap-2">
         <LanguageSwitcher />
-
         {!user ? (
           <>
-            <Link href={routes.signin}>{t('signIn')}</Link>
-            <Link href={routes.signup}>{t('signUp')}</Link>
+            <Link href={routes.signin} className="custom-button">
+              {t('signIn')}
+            </Link>
+            <Link href={routes.signup} className="custom-button">
+              {t('signUp')}
+            </Link>
           </>
         ) : (
           <div>
-            <Button color="inherit" onClick={onLogout}>
+            <span color="inherit" onClick={onLogout} className="custom-button cursor-pointer">
               {t('signOut')}
-            </Button>
+            </span>
           </div>
         )}
       </div>
